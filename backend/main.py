7b4b9653650b66129f8606f19063927cd8815382
Marlon Marin <marlonmariin97@outlook.com>
2025-06-08 18:12:20 -0500

    (new) Setting up a basic main.py backend file

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify #type: ignore
+from flask_cors import CORS # type: ignore
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*" }})
+# CORS(app, resources={r"/*": {
+#     "origins": "http://localhost:8080", 
+#     "allow_headers" : "Access-Control-Allow-Origin"
+#         }
+#     }
+# )
+
+@app.route('/', methods=['GET'])
+def greetings():
+    return ("Hello, World! This is the backend of the application.", 200)
+
+if __name__ == '__main__':
+    app.run(debug=True)
